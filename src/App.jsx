c16d084fa939b2b8961564910968f3ad16fa8718
Tariq Sulley <tariqsulley3c@gmail.com>
2023-01-06 21:04:38 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
 import { BrowserRouter,Route,Routes } from "react-router-dom";
 import Login from './pages/Login';
 function App() {
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,28 @@
 import React from "react"
+import "../styles/LoginStyles.css"
+import svg from "../assets/engineers.svg"
 
 const Login =()=>{
     return(
-        <div>
-            Login
+        <div className="MainPage">
+            <div className ="RightSide">
+                <div className="bar">
+                
+                </div>
+                <div className="Welcome-Txt">
+                    Welcome To The CENG Portal
+                </div>
+                <div className="Submit-Txt">
+                    Submit your ceng topics for approval and reports for
+                    Grading by Lecturers
+                </div>
+                <div>
+                    <img className="Svg" src={svg}/>
+                </div>
+            </div>
+            <div className="LeftSide">
+                input
+            </div>
         </div>
     )
 }
