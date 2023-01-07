514a5d57bfe0ae72229abdaf54417daf667410dc
Tariq Sulley <tariqsulley3c@gmail.com>
2023-01-07 02:24:49 +0000

    changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,14 +2,27 @@ import React, {useEffect,useState} from "react"
 import "../styles/LoginStyles.css"
 import LeftDisplay from "../components/LeftDisplay"
 
+const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const Login =()=>{
     const [email,setEmail] = useState("")
     const [password,setPassword] = useState("")
+    const [passError,setPassError] = useState("")
+    const [emailError,setEmailError] = useState(null)
 
+    const validateEmail = (email) =>{
+        if(emailRegex.test(email)){
+                setEmailError(false)
+        }
+        else{
+            setEmailError(true)
+        }
+    }
+    
     useEffect(()=>{
-        
-    })
+        setPassError("")
+    },[])
+
     return(
         <div className="MainPage">
             <LeftDisplay/>
@@ -26,8 +39,11 @@ const Login =()=>{
                <form label="login-form" className="form">
                 <div className="Email-Field">
                      <div className="Email-Txt">Email Address</div>
-                    <input value={email} onChange={(e)=> setEmail(e.target.value)} 
-                    className="Email-Input" type="text"/>
+                    <input value={email} onChange={(e)=> 
+                    {setEmail(e.target.value);validateEmail(e.target.value)}} 
+                    className={(emailError == null ? "Email-Input":(emailError == true ? "Email-Input-Bad":"Email-Input-Valid") )
+                     } 
+                    type="text"/>
                 </div>
                 <div className="Password-Field">
                     <div className="Password-Top">
@@ -38,9 +54,10 @@ const Login =()=>{
                             <div className="Forgot-Txt">Forgot Password?</div>
                         </div>
                     </div>
-                    <input value={password} onChange={(e)=> setPassword(e.target.value)}
+                    <input value={password} onChange={(e)=> {setPassword(e.target.value);
+                    setPassError("Password cannot be empty")}}
                      className="Password-Input" type="password"/>
-                     <p className="empty">{password.length > 0 ? "":"Password cannot be empty"}</p>
+                     <p className="empty">{password.length > 0 ? "":passError}</p>
                 </div>
                 <button className="Btn"> Sign In </button>
                </form>
