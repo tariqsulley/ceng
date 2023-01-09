b7b0732133548be9e30a9157e9daedc3f3bfca9c
Tariq Sulley <tariqsulley3c@gmail.com>
2023-01-09 16:46:10 +0000

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import "../styles/HomeStyles.css"
 import { UserAuth } from "../contexts/AuthContext";
 import { useNavigate } from "react-router";
 import SubmitPage from "./Submit";
 
 const Home = ()=>{
+    const[page,setPage] = useState()
     const {user,logOut} = UserAuth()
     const navigate = useNavigate()
 
@@ -31,7 +32,7 @@ const Home = ()=>{
                <p className="Coe-Txt">CoE Ceng Portal</p>
                </div>
                <div className="Home-Field">
-                <p className="Student-Txt">Student Home</p>
+                <p onClick={()=>setPage(1)} className="Student-Txt">Student Home</p>
                </div>
                </div>
 
@@ -40,10 +41,10 @@ const Home = ()=>{
                         Tools
                     </div>
                     <div className="Submit-Field">
-                        <p className="Student-Txt">Submit Your Topic</p>
+                        <p onClick={()=>setPage(2)} className="Student-Txt">Submit Your Topic</p>
                     </div>
                     <div className="Upload-Field">
-                        <p className="Student-Txt">Upload Your Report</p>
+                        <p onClick={()=>setPage(3)} className="Student-Txt">Upload Your Report</p>
                     </div>
                </div>
                </div>
@@ -55,8 +56,7 @@ const Home = ()=>{
                </div>
             </div>
             <div className="Content">
-           
-            <SubmitPage/>
+             {page === 1 ? <p>1</p>:page === 2 ? <SubmitPage/>:3 }
             </div>
         </div>
     )
