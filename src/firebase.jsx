7d5bf166a8ae856e8fa128ee9f51a704f919d525
tariqsulley <32623579+tariqsulley@users.noreply.github.com>
2023-01-12 23:10:51 +0000

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,8 +1,10 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
+import {getFirestore} from "firebase/firestore" 
+
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
+  apiKey: import.meta.env.VITE_apiKey, 
   authDomain: import.meta.env.VITE_authDomain,
   projectId: import.meta.env.VITE_projectId,
   storageBucket: import.meta.env.VITE_storageBucket,
@@ -13,4 +15,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+export const db = getFirestore(app)
+
 export default app
+
+
