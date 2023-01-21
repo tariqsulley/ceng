2a0b574ec0170a5585c0ed935c78c9feef0b0d08
tariqsulley <32623579+tariqsulley@users.noreply.github.com>
2023-01-21 21:19:21 +0000

    Changes

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -56,7 +56,7 @@ const Card = ()=>{
     }else{
         return(
             <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-                <CircularProgress style={{color:"orange"}}/>
+                <CircularProgress style={{color:"gray"}}/>
             </div>
         )
     }
